d26251c60e082e0c9c9f2998884e42fa5f9d8ab5
nicofilizzola <nicofilizzola.prod@gmail.com>
2021-03-13 17:41:23 +0100

    style on input searchbar

--- public/js/routes/attribution/index.js
+++ public/js/routes/attribution/index.js
@@ -1,8 +1,5 @@
-// Professor searchbar
+// Attribution searchbar
 var professorSearchbar = document.getElementById('js-professor-searchbar');
 var attributionResults = document.querySelectorAll('.js-attribution-result');
 searchbar(professorSearchbar, attributionResults);
 
-// Subject searchbar
-var subjectSearchbar = document.getElementById('js-subject-searchbar');
-searchbar(subjectSearchbar, attributionResults);
